05a93af2d7d7426ee14504e8c8cfc800cc68ed38
zoey <dinuoz@uw.edu>
2025-05-23 12:01:48 -0700

    Unnecessry folders removed

--- src/data_augmentation/marian/20_each_language/20_each_language.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-exec ~/miniconda3/envs/nllb/bin/python 20_each_language.py\
-    --input    "$1" \
-    --output   "$2" \
-    --use_gpu "$3"
--- src/data_augmentation/nllb/translate_full/translate_full.sh
+++
@@ -1,4 +0,0 @@
-exec ~/miniconda3/envs/nllb/bin/python translate_full.py\
-    --input    "$1" \
-    --output   "$2" \
-    --use_gpu "$3"
